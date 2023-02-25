555187cf81405f6bad05f1d3ea9428440c88a192
denis-gr <qwertyuiop852002058@yandex.com>
2023-02-25 17:35:10 +0000

    commit

---
+++ videos/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+from .models import VideoPage, MainPage
+
+admin.site.register(VideoPage)
+admin.site.register(MainPage)
--- videos/models.py
+++ videos/models.py
@@ -1,5 +1,12 @@
+import io
+import os
+import tempfile
+
+
 import cv2
 from django.db import models
+from django.core.files import File
+
 
 from wagtail.models import Page
 from wagtail.admin.panels import FieldPanel
@@ -13,11 +20,9 @@ class VideoPage(Page):
     description = models.TextField(verbose_name="Описание", blank=True, null=True)
     image = models.ImageField(verbose_name="Превью", blank=True, null=True)
     original_video = models.FileField(verbose_name="Оригинал видео")
-    original_size_width = models.IntegerField(blank=True, null=True)
-    original_size_height = models.IntegerField(blank=True, null=True)
+    original_width = models.IntegerField(blank=True, null=True)
+    original_height = models.IntegerField(blank=True, null=True)
     duration = models.IntegerField(blank=True, null=True)
-    video_144 = models.FileField(verbose_name="144", blank=True, null=True)
-    video_240 = models.FileField(verbose_name="240", blank=True, null=True)
     video_360 = models.FileField(verbose_name="360", blank=True, null=True)
     video_480 = models.FileField(verbose_name="480", blank=True, null=True)
     video_720 = models.FileField(verbose_name="720", blank=True, null=True)
@@ -25,9 +30,6 @@ class VideoPage(Page):
 
     search_fields = Page.search_fields + [
         index.SearchField("description"),
-        index.FilterField("original_size_width"),
-        index.FilterField("original_size_height"),
-        index.FilterField("duration"),
     ]
     content_panels = Page.content_panels + [
         FieldPanel("original_video"),
@@ -41,20 +43,37 @@ class VideoPage(Page):
     subpage_types = []
 
     def save(self, **kwargs):
-        old = self.__class__.objects.get(id=self.id).specific.original_video
-
+        result = super().save(**kwargs)
         cap = cv2.VideoCapture(self.original_video.path)
-        self.original_size_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-        self.original_size_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-        self.duration = int(cap.get(cv2.CAP_PROP_POS_MSEC))
+        self.original_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+        self.original_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        fps = cap.get(cv2.CAP_PROP_FPS)
+        self.duration = cap.get(cv2.CAP_PROP_FRAME_COUNT) // fps
 
         # get and swet image
         image = cap.read()[-1]
         io_buf = io.BytesIO(cv2.imencode(".jpg", image)[-1])
-        self.image = File(io_buf)
-
-        if old.video != self.video:
-            ...
-        result = super().save(**kwargs)
+        self.image = File(io_buf, name=self.title+"_image.jpg")
+        
+        # get video with differnet sizes
+        sizes = [(640, 360), (854, 480), (1280, 720), (1920, 1080)]
+        sizes = filter(lambda x: min(x) <= min([self.original_width, self.original_height]), sizes)
+        out_files = { i: tempfile.mkstemp(suffix = ".avi") for i in sizes}
+        video_cod = cv2.VideoWriter_fourcc(*'XVID')
+        out_writers = { i: cv2.VideoWriter(j[1], video_cod, fps, i)
+            for i, j in out_files.items() }
+        while(cap.isOpened()):
+            ret, frame = cap.read()
+            if ret==True:
+                for i, j in out_writers.items():
+                    j.write(cv2.resize(frame, i))
+            else:
+                break
+        for i in out_writers.values(): i.release()
+        for i, j in out_files.items():
+            name = f"video_{min(i)}"
+            setattr(self, name, File(open(j[1], mode="rb"), name=f"{self.title}_{name}.avi"))
+            os.close(j[0])
+        cap.release()
         return result
 
