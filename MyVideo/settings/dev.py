842be48e221fa43a5482f19d490849cda1f83239
denis-gr <qwertyuiop852002058@yandex.com>
2023-03-07 00:35:01 +0000

    ...

--- MyVideo/settings/dev.py
+++ MyVideo/settings/dev.py
@@ -8,13 +8,13 @@ SECRET_KEY = "django-insecure-lgy3o#uy#8-g7z!q&zj$q&xrx5)os^k7dfnz*5+rc94xnqdgja
 
 # SECURITY WARNING: define the correct hosts in production!
 ALLOWED_HOSTS = ["*"]
-CSRF_TRUSTED_ORIGINS = ["https://*.gitpod.io/"]
+#CSRF_TRUSTED_ORIGINS = ["https://*.gitpod.io/"]
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
-CORS_ALLOWED_ORIGINS = [
-    'https://5173-denisgr-myvideofront-cne3wanp45m.ws-eu89.gitpod.io',
-]
+#CORS_ALLOWED_ORIGINS = [
+#    'https://5173-denisgr-myvideofront-cne3wanp45m.ws-eu89.gitpod.io',
+#]
 
 try:
     from .local import *
